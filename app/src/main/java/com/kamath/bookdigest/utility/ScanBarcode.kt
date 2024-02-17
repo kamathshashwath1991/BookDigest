a6db2324ac2ef12a4f9f9713fb287b4f9821011a
Shashwath Kamath <kamathsh91@gmail.com>
2024-02-17 16:11:54 -0500

    code clean up in scan barcode tab

--- app/src/main/java/com/kamath/bookdigest/utility/ScanBarcode.kt
+++ app/src/main/java/com/kamath/bookdigest/utility/ScanBarcode.kt
@@ -1,14 +1,8 @@
 package com.kamath.bookdigest.utility
-
-import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
@@ -21,12 +15,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.kamath.bookdigest.ui.screens.common.BookDetails
 import com.kamath.bookdigest.viewModels.BooksViewModel
 import kotlinx.coroutines.launch
-import kotlin.math.log
 
 @Composable
 fun ScanBarcode(
