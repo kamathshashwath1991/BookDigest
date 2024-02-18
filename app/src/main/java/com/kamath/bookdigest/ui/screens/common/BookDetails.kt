7b8cac2031ac4a885ac47c96ad3fad42dc8aa202
Shashwath Kamath <kamathsh91@gmail.com>
2024-02-17 19:18:59 -0500

    common component for UI

--- app/src/main/java/com/kamath/bookdigest/ui/screens/common/BookDetails.kt
+++ app/src/main/java/com/kamath/bookdigest/ui/screens/common/BookDetails.kt
@@ -1,12 +1,20 @@
 package com.kamath.bookdigest.ui.screens.common
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text2.BasicTextField2
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import com.kamath.bookdigest.data.model.Book
 
@@ -22,10 +30,15 @@ fun BookDetails(book:Book){
         // Add more book details as needed
     }
 }
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun BookDetailItem(label: String, value: String) {
-    Text(
-        text = "$label: $value",
-        modifier = Modifier.padding(vertical = 4.dp)
+
+    Text(text = label, style =  TextStyle(Color.Green))
+    BasicTextField2(
+        value = value,
+        onValueChange = {},
+        readOnly = true,
+        textStyle = TextStyle(Color.White)
     )
 }
