150ddc718b8cf2759a27d1c99f1380992518521b
Shashwath Kamath <kamathsh91@gmail.com>
2024-02-17 19:19:46 -0500

    cleaning code

--- app/src/main/java/com/kamath/bookdigest/ui/screens/common/BookDetails.kt
+++ app/src/main/java/com/kamath/bookdigest/ui/screens/common/BookDetails.kt
@@ -3,14 +3,9 @@ package com.kamath.bookdigest.ui.screens.common
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text2.BasicTextField2
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
