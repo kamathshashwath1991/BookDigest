bfca4d609d034cc2f35f4495e08200b500d910f9
Shashwath Kamath <kamathsh91@gmail.com>
2024-04-01 20:41:30 -0400

    code clean

--- app/src/main/java/com/kamath/bookdigest/ui/screens/AccountScreen.kt
+++ app/src/main/java/com/kamath/bookdigest/ui/screens/AccountScreen.kt
@@ -1,14 +1,10 @@
 package com.kamath.bookdigest.ui.screens
 
-import android.app.LauncherActivity
-import android.app.LauncherActivity.ListItem
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.ListItem
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
