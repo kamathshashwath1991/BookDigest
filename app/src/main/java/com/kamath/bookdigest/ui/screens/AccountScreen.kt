a00dd8c24242ff47616a161f1e5cb7a7c065ddd5
Shashwath Kamath <kamathsh91@gmail.com>
2024-04-01 20:40:54 -0400

    few more components

--- app/src/main/java/com/kamath/bookdigest/ui/screens/AccountScreen.kt
+++ app/src/main/java/com/kamath/bookdigest/ui/screens/AccountScreen.kt
@@ -1,16 +1,21 @@
 package com.kamath.bookdigest.ui.screens
 
+import android.app.LauncherActivity
+import android.app.LauncherActivity.ListItem
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ListItem
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.kamath.bookdigest.ui.screens.accountScreenComponents.ButtonsRow
 import com.kamath.bookdigest.ui.screens.accountScreenComponents.FollowersRow
 import com.kamath.bookdigest.ui.screens.accountScreenComponents.ProfileImageAndName
+import com.kamath.bookdigest.ui.screens.accountScreenComponents.SubList
 
 @Composable
 fun AccountScreen(){
@@ -27,6 +32,16 @@ fun AccountScreen(){
         )
         Spacer(modifier = Modifier.height(16.dp))
         FollowersRow(bookListed = 100, following = 200, followers = 300)
+        Spacer(modifier = Modifier.height(16.dp))
+
+        SubList(
+            sectionTitle = "Book Listing",
+            items = listOf("Book 1", "Book 2", "Book 3"), // Example book items
+            onItemClick = { itemName ->
+                // Handle item click
+            }
+        )
+
     }
 
 }
